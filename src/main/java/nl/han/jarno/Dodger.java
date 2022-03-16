60f4e08ded8fdb7b53f70b7eda1b7fca7e783df2
Bonjeurno <jarnovos01@hotmail.com>
2022-03-16 10:13:49 +0100

    Added some useless JavaDoc

--- src/main/java/nl/han/jarno/Dodger.java
+++ src/main/java/nl/han/jarno/Dodger.java
@@ -2,6 +2,9 @@ package nl.han.jarno;
 
 import com.github.hanyaeger.api.YaegerGame;
 
+/**
+ * Het startpunt van onze GAME
+ */
 public class Dodger extends YaegerGame {
 
     public static void main(String[] args){
